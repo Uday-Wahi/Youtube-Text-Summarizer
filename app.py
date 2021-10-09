efad27763b2ad3d1594414383c7e6956637b2f51
Uday Wahi <hiudaywahi@gmail.com>
2021-10-09 18:26:48 +0530

    summarize endpoint completed

--- app.py
+++ app.py
@@ -1,17 +1,54 @@
-from flask import Flask
-import datetime
 from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+from flask import Flask, request
 
-# define a variable to hold you app
+
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
 app = Flask(__name__)
 
 
-# define your resource endpoints
-@app.get('/')
-def index_page():
-    return "Hello World"
+def getTranscript(id: str) -> list:
+    return YouTubeTranscriptApi.get_transcript(id)
 
 
-# start the server on localhost:8000
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+def extractText(dictionary: list) -> str:
+    text = ""
+    for d in dictionary:
+        text = text + d['text'] + " "
+    return text
+
+
+def cleanText(text: str) -> str:
+    cleanedText = " ".join(text.split())
+    return cleanedText
+
+
+def summarizeText(longText: str) -> str:
+    inputs = tokenizer.encode("summarize: " + longText, return_tensors="pt")
+    outputs = model.generate(
+        inputs,
+        num_beams=4,
+        no_repeat_ngram_size=2,
+        min_length=200,
+        max_length=300,
+        early_stopping=True
+    )
+    decoded = tokenizer.decode(outputs[0])
+    return decoded
+
+
+# Index page
+@app.route("/")
+def index():
+    return "<h2>Goto /api/summarize?videoID=<youtube_video_id></h2>"
+
+
+# api call to summarize
+@app.route("/api/summarize")
+def summarize() -> str:
+    return summarizeText(cleanText(extractText(getTranscript(request.args.get("videoID")))))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host='0.0.0.0')
